8a1bad951080581b7a47c3c331df145b1b33628d
Fernando Judice <fernandocesar95@gmail.com>
2024-04-27 22:09:54 -0300

    create basic math operations

---
+++ index.js
@@ -0,0 +1,19 @@
+function add(a,b){
+    return a+b
+}
+
+function subtract(a,b){
+    return a-b
+}
+
+function multiple(a,b){
+    return a*b
+}
+
+function divide(num,den){
+    if (den === 0) {
+        return "ERROR - Cannot divide by zero"
+    }
+    return num/den
+}
+
