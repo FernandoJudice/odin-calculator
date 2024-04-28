d678800b4c257cd0ba99e021311154efbb7d37fa
Fernando Judice <fernandocesar95@gmail.com>
2024-04-27 22:52:47 -0300

    add numeral buttons render function

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
-let firstValue;
+let accumulator;
 let operator;
-let secondValue;
+let buffer;
+
+const left = document.querySelector(".left");
+
+renderOneToNine();
+renderZero();
+
+function renderOneToNine(){
+    for (let i=0; i<3; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
+        left.appendChild(row);
+
+        for (let j=0;j<3;j++) {
+            const calcButton = document.createElement("div");
+            calcButton.classList.add("calcButton");
+            calcButton.style.flex = "1 0 auto";
+            calcButton.textContent = i*3+j+1;
+            row.appendChild(calcButton);
+        }
+    }
+}
+
+function renderZero(){
+    const calcButton = document.createElement("div");
+    calcButton.classList.add("calcButton");
+    calcButton.style.flex = "2 0 auto";
+    calcButton.textContent = 0;
+    left.appendChild(calcButton);
+}
+
+
 
 function operate(operator,firstValue,secondValue){
     return operator(firstValue,secondValue);
